375bd8f5980de09f6ec610c5c06077aa912a01d0
Shivam Sharma <sharma.shivam@indiamart.com>
2019-12-17 13:37:16 +0530

    Added ICON and icon folders temprarily

--- modules/view/index.php
+++ modules/view/index.php
@@ -2,18 +2,18 @@
 <html lang="en" dir="ltr">
 	<head>
 		<meta charset="utf-8">
-		<title>This file is mine</title>
+		<title>This File Is Mine</title>
 	</head>
 	<body>
 		<header>
-			<img src="/assets/img/logo-32x32.png" alt="TFIM">
+			<img src="assets/img/logo-32x32.png" alt="TFIM">
 			<span class="webTitle">This File Is Mine</span>
 		</header>
 		<section>
 			
 		</section>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-		<link rel="stylesheet" href="/assets/css/main.css">
+		<link rel="stylesheet" href="assets/css/main.css">
 		<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 
 	</body>

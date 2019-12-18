24012a3197447368f828ffbf190908e4622ffb32
Shivam Sharma <sharma.shivam@indiamart.com>
2019-12-18 11:15:52 +0530

    Added Header and Login Buttons

--- modules/view/index.php
+++ modules/view/index.php
@@ -5,9 +5,23 @@
 		<title>This File Is Mine</title>
 	</head>
 	<body>
-		<header>
-			<img src="assets/img/logo-32x32.png" alt="TFIM">
-			<span class="webTitle">This File Is Mine</span>
+		<header class="container-fluid">
+			<div class="row">
+				<div class="col-xs-4">
+					<img src="assets/img/logo-32x32.png" alt="TFIM">
+					<span class="webTitle">This File Is Mine</span>
+				</div>
+				<nav class="col-xs-8">
+					<ul>
+						<li>
+							<button class="btn btn-default" type="button" name="signup">Sign Up</button>
+						</li>
+						<li>
+							<button class="btn btn-primary" type="button" name="login">Log In</button>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 		<section>
 			
@@ -15,6 +29,5 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<link rel="stylesheet" href="assets/css/main.css">
 		<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-
 	</body>
 </html>

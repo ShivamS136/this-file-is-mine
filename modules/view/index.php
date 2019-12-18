2a21c818f2a1310c849a30d007b086bb8761fae9
Shivam Sharma <sharma.shivam@indiamart.com>
2019-12-18 15:47:47 +0530

    Changed grid system from Bootstrap to Flexbox

--- modules/view/index.php
+++ modules/view/index.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta charset="utf-8">
 		<title>This File Is Mine</title>
 	</head>
 	<body>
-		<header class="container-fluid">
-			<div class="row">
-				<div class="col-xs-4">
-					<img src="assets/img/logo-32x32.png" alt="TFIM">
-					<span class="webTitle">This File Is Mine</span>
-				</div>
-				<nav class="col-xs-8">
-					<ul>
-						<li>
-							<button class="btn btn-default" type="button" name="signup">Sign Up</button>
-						</li>
-						<li>
-							<button class="btn btn-primary" type="button" name="login">Log In</button>
-						</li>
-					</ul>
-				</nav>
+		<header class="flex-container">
+			<div>
+				<img src="assets/img/logo-32x32.png" alt="TFIM">
+				<span class="webTitle">This File Is Mine</span>
 			</div>
+			<nav>
+				<ul>
+					<li>
+						<button class="btn btn-default" type="button" name="signup">Sign Up</button>
+					</li>
+					<li>
+						<button class="btn btn-primary" type="button" name="login">Log In</button>
+					</li>
+				</ul>
+			</nav>
 		</header>
 		<section>
 			

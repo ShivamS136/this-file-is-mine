23490ec4c4b2d55400867ef57838e41d59c28ad0
Shivam Sharma <shivamshrm235@gmail.com>
2020-10-01 21:24:40 +0530

    Refreshed the whole project

--- modules/view/header.php
+++ modules/view/header.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta charset="utf-8">
-		<title>This File Is Mine</title>
-		<link rel="stylesheet" href="/public/css/bootstrap.min.css">
-		<link rel="stylesheet" href="/public/css/main.css">
-		<script src="public/js/jquery.min.js"></script>
-		<script src="public/js/bootstrap.bundle.min.js"></script>
-		<script src="public/js/main.js"></script>
-	</head>
-	<body>
-		<header>
-			
-		</header>
-		<section>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<title>This File Is Mine</title>
+	<link rel="stylesheet" href="/public/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/public/css/main.css">
+	<script src="public/js/jquery.min.js"></script>
+	<script src="public/js/bootstrap.bundle.min.js"></script>
+	<script src="public/js/main.js"></script>
+</head>
+<body>
+	<header>
+		
+	</header>
+	<section>

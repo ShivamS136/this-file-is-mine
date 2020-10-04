d0508742b5a7b6786f8ff9bafbe804b0d97e3cd2
Shivam Sharma <shivamshrm235@gmail.com>
2020-10-04 20:12:22 +0530

    Added basic lock files

--- modules/view/index.php
+++ modules/view/index.php
@@ -4,18 +4,18 @@
 ?>
 	<div class="container-fluid p-3">
 		<div class="row m-0 py-3 bg-light mb-3">
-			<div class="col-12 col-md-3 border-right">
+			<div class="col-12 col-sm-3 border-right">
 				<div class="nav flex-column nav-pills" role="tablist" aria-orientation="vertical">
 					<a class="nav-link active" id="lockFilesTab" data-toggle="pill" href="#lockFiles" role="tab" aria-controls="lockFiles" aria-selected="true">Lock Files</a>
 					<a class="nav-link" id="releaseFilesTab" data-toggle="pill" href="#releaseFiles" role="tab" aria-controls="releaseFiles" aria-selected="false">Release Files</a>
 					<a class="nav-link" id="getFilesStatusTab" data-toggle="pill" href="#getFilesStatus" role="tab" aria-controls="getFilesStatus" aria-selected="false">Get Files' Status</a> 
 				</div>  
 			</div>
-			<div class="col-12 col-md-9">
+			<div class="col-12 col-sm-9">
 				<div class="tab-content">
 					<div id="formProject">
 						<div class="form-group row">
-							<label for="projects" class="col-sm-3 col-form-label text-right">Project:</label>
+							<label for="projects" class="col-sm-3 col-form-label text-sm-right">Project:</label>
 							<div class="col-sm-9" id="projectsDiv" style="max-width:400px;">
 								<select class="custom-select" id="projects"></select>
 								<p class="msg text-muted d-none"></p>
@@ -29,16 +29,14 @@
 					</div>
 				<div class="tab-pane fade show active" id="lockFiles" role="tabpanel" aria-labelledby="lockFilesTab">
 					<div class="form-group row">
-						<label for="lockFilesList" class="col-sm-3 col-form-label text-right">List of files:</label>
+						<label for="lockFilesList" class="col-sm-3 col-form-label text-sm-right">List of files:</label>
 						<div class="col-sm-9" id="lockFilesListDiv" style="max-width:400px;">
 							<textarea id="lockFilesList" class="form-control" rows="3" data-gramm_editor="false" data-gramm="false" spellcheck="false"></textarea>
 							<p class="msg text-muted d-none"></p>
 						</div>
 					</div>
 					<div class="form-group row justify-content-center">
-						<div class="col-3">
-							<button class="btn btn-outline-primary" id="lockFilesBtn" data-toggle="modal" data-target="#lockFilesModal">Lock Files</button>
-						</div>
+						<button class="btn btn-outline-primary" id="lockFilesBtn" data-toggle="modal" data-target="#lockFilesModal">Lock Files</button>
 					</div>
 				</div>
 				<div class="tab-pane fade" id="releaseFiles" role="tabpanel" aria-labelledby="releaseFilesTab">Release Files</div>
